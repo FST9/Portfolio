448959e6de156aee49300a4b43a08b1495ae8b64
samuel <temidayofalade092@gmail.com>
2025-07-10 12:34:29 +0100

    first commit

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+//navlinks togglebar functionality
+function toggleBar() {
+  const navLinks = document.querySelector(".nav-links");
+  if (navLinks.style.display === "flex") {
+    navLinks.style.display = "none";
+  } else {
+    navLinks.style.display = "flex";
+  }
+}
+//Blog section filter functionality
+function showall() {
+  document.getElementById("aboutp").style.display = "block";
+  document.getElementById("servicesp").style.display = "block";
+  document.getElementById("contactp").style.display = "block";
+  document.getElementById("projectsp").style.display = "block";
+}
+function showaboutp() {
+  document.getElementById("aboutp").style.display = "block";
+  document.getElementById("servicesp").style.display = "none";
+  document.getElementById("contactp").style.display = "none";
+  document.getElementById("projectsp").style.display = "none";
+}
+function showservicesp() {
+  document.getElementById("aboutp").style.display = "none";
+  document.getElementById("servicesp").style.display = "block";
+  document.getElementById("contactp").style.display = "none";
+  document.getElementById("projectsp").style.display = "none";
+}
+function showprojectsp() {
+  document.getElementById("aboutp").style.display = "none";
+  document.getElementById("servicesp").style.display = "none";
+  document.getElementById("contactp").style.display = "none";
+  document.getElementById("projectsp").style.display = "block";
+}
+function showcontactp() {
+  document.getElementById("aboutp").style.display = "none";
+  document.getElementById("servicesp").style.display = "none";
+  document.getElementById("contactp").style.display = "block";
+  document.getElementById("projectsp").style.display = "none";
+}
